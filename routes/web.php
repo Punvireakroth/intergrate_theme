1cf8119933da6ae9e0a59292a3a3fac72e3ee838
Pun VireakRoth <vireakrothpun@gmail.com>
2025-04-26 22:07:00 +0700

    Intial commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+Route::redirect('/', '/home');
+Route::redirect('/dashboard', '/home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', function () {
+        return view('layouts.home');
+    });
+
+
+    Route::get('/about', function () {
+        return view('layouts.about');
+    });
+
+    Route::get('/services', function () {
+        return view('layouts.services');
+    });
+
+    Route::get('/contact', function () {
+        return view('layouts.contact');
+    });
+
+    Route::get('/blog', function () {
+        return view('layouts.blog');
+    });
+});
